4ce04df09be41d0046dff85fab08ea1fe96de470
FIREBEAR\Lucas <lucas.randria@hotmail.fr>
2016-10-08 14:42:06 +0200

    Link JavaScript and CSS files

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+
+$(function() {                        
+	$('.PI_hexa').click(function () {
+		$('.page').fadeIn(200);
+		$('.topbar').fadeIn(200);
+		$('.main-link').hide();					  				   						
+	});
+});									
