48cd2419dad13bd584ef385078c393fe20411a0b
FIREBEAR\Lucas <lucas.randria@hotmail.fr>
2016-10-23 15:18:40 +0200

    UPDATE background changing color

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@ $(function() {
     $('.about-me-small .title').click(function () {
         if($('.about-me-small .sub').css('display') == 'none'){
             $('.about-me-small .sub').slideDown("normal");
+            $(".content").css('background-color', '#F5D76E');
         }else{
             $('.about-me-small .sub').slideUp("none");
         }
@@ -28,6 +29,7 @@ $(function() {
         {
             if($('.formation .sub').css('display') == 'none'){
                 $('.formation .sub').slideDown("normal");
+                $(".content").css('background-color', '#3FC380');
             }else{
                 $('.formation .sub').slideUp("none");
             }
@@ -42,6 +44,7 @@ $(function() {
         {
             if($('.XP .sub').css('display') == 'none'){
                 $('.XP .sub').slideDown("normal");
+                $(".content").css('background-color', '#EB974E');
             }else{
                 $('.XP .sub').slideUp("none");
             }
@@ -56,6 +59,7 @@ $(function() {
         {
             if($('.skills .sub').css('display') == 'none'){
                 $('.skills .sub').slideDown("normal");
+                $(".content").css('background-color', '#3498DB');
             }else{
                 $('.skills .sub').slideUp("none");
             }
@@ -70,6 +74,7 @@ $(function() {
         {
             if($('.hobbies .sub').css('display') == 'none'){
                 $('.hobbies .sub').slideDown("normal");
+                $(".content").css('background-color', '#BDC3C7');
             }else{
                 $('.hobbies .sub').slideUp("none");
             }
@@ -84,6 +89,7 @@ $(function() {
         {
             if($('.contact .sub').css('display') == 'none'){
                 $('.contact .sub').slideDown("normal");
+                $(".content").css('background-color', '#BDC3C7');
             }else{
                 $('.contact .sub').slideUp("none");
             }
@@ -94,5 +100,46 @@ $(function() {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
     });
+
+        var scroll_pos = 0;
+        var wHeight = $(window).height();
+        var wWidth = $(window).width();
+        var pos_about = wHeight * 0.5;
+        var pos_formation = wHeight * 1;
+        var pos_xp = wHeight * 2.2;
+        var pos_skills = wHeight * 3.5;
+        var pos_hobbies = wHeight * 4;
+
+
+        $(document).scroll(function() {
+            scroll_pos = $(this).scrollTop();
+
+            if(wWidth >= 992){
+                if(scroll_pos < pos_about) {
+                    $(".content").css('background-color', '240,240,240');
+                } else {
+                    if(scroll_pos >= pos_about && scroll_pos < pos_formation){
+                        $(".content").css('background-color', '#F5D76E');
+                    } else {
+                        if (scroll_pos >= pos_formation && scroll_pos < pos_xp) {
+                            $(".content").css('background-color', '#3FC380');
+                        } else {
+                            if (scroll_pos >= pos_xp && scroll_pos < pos_skills) {
+                                $(".content").css('background-color', '#EB974E');
+                            } else {
+                                if (scroll_pos >= pos_skills && scroll_pos < pos_hobbies) {
+                                    $(".content").css('background-color', '#3498DB');
+                                } else {
+                                    if (scroll_pos >= pos_hobbies) {
+                                        $(".content").css('background-color', '#BDC3C7');
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+        });
 
 });									
